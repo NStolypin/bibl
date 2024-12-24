e487de5a83147c1fec963e4350dbde45544ec4e7
NStolypin <i@nstolypin.ru>
2024-12-25 00:44:39 +0300

    fix 4

---
+++ src/main/java/ru/esplit/dao/PersonDAOImpl.java
@@ -0,0 +1,39 @@
+package ru.esplit.dao;
+
+import java.util.List;
+
+import ru.esplit.models.Person;
+
+public class PersonDAOImpl implements PersonDAO {
+
+    @Override
+    public List<Person> getAll() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getAll'");
+    }
+
+    @Override
+    public Person show(int id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'show'");
+    }
+
+    @Override
+    public void save(Person person) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'save'");
+    }
+
+    @Override
+    public void update(int id, Person updatePerson) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'update'");
+    }
+
+    @Override
+    public void delete(int id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'delete'");
+    }
+    
+}
